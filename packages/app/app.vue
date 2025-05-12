c74b13a0eea015d6ba8fdf522c8a1fcbd9288d2d
Tomislav Glavaš <tomo.glavas@gmail.com>
2025-05-12 22:22:13 +0200

    Init repository files

---
+++ packages/app/app.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import type { UiTheMessage } from '#components'
+import { baseUrl } from '@nuxt-monorepo/api'
+
+const theMessage = ref<InstanceType<typeof UiTheMessage> | null>()
+
+onMounted(() => {
+  console.log('theMessage', theMessage.value)
+})
+
+const config = useRuntimeConfig()
+</script>
+
+<template>
+  <div>
+    {{ config.public.apiBase }}
+    <UiTheMessage ref="theMessage" />
+    api/baseUrl: {{ baseUrl }}
+  </div>
+</template>
---
+++ packages/ui/components/TheMessage.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { message } from '~ui/constants/message'
+</script>
+
+<template>
+    <div>
+        Component from ui {{ message }}
+    </div>
+</template>
